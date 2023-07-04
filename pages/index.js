e35fb8c38d749687d6978d512da8a50ef9447c1c
Ishu Pathak <77011944+Ishukumarpathak@users.noreply.github.com>
2023-07-04 12:52:30 +0530

    product card and grid completed

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import HeroBanner from "@/components/HeroBanner";
 import Wrapper from "@/components/Wrapper";
+import ProductCard from "@/components/ProductCard";
 export default function Home() {
   return (
     <main>
       <HeroBanner />
-      <Wrapper />
+      <Wrapper>
       <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
         <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
           MAKING A MASTERPIECE
@@ -19,6 +20,19 @@ export default function Home() {
           the Nippon Made.
         </div>
       </div>
+
+      <div className="grid grid-cols-1 md:geid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+      </div>
+      </Wrapper>
     </main>
   );
 }
